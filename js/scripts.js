0dc711ad9af0734f71c8c5d57eb8c650b70bf679
Clemo97 <lumumbaclement@gmail.com>
2022-03-10 22:12:43 +0300

    change css element with jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
-.$(document).ready(function () {
-    $("button#green").click(function () { 
-        .$("body").addClass("green-background");
-    });
-    $("button#yellow").click(function () {
-        .$("body").addClass("yellow-bakcground");
-    });
-    .$("button#red").click(function () { 
-        
-    });
+$(document).ready(function () {
+  $("button#green").click(function () {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function () {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function () {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
